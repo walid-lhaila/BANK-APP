1cd2522ed3106c0d96676dead612d5802192ad78
walid-lhaila <walidlhaila00@gmail.com>
2023-11-24 17:35:25 +0100

    brief6

--- transactionClient.php
+++ transactionClient.php
@@ -1,3 +1,53 @@
+<?php
+        include("datacnx.php");
+
+
+  //create table
+
+  $sql = "CREATE TABLE IF NOT EXISTS transactions (
+    id INT(20) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    montant VARCHAR(30) NOT NULL,
+    types VARCHAR(30) NOT NULL,
+    compt_id INT(20) UNSIGNED NOT NULL,
+    FOREIGN KEY (compt_id) REFERENCES account(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+    )";
+
+
+
+          $cnx-> query($sql);
+          
+
+          if ($cnx->errno) {
+            echo "Error creating table: " . $cnx->error;
+        } else {
+            echo "Table created successfully";
+        }
+
+            //insert
+
+            if (isset($_POST["submit"])) {
+              $montant = isset($_POST['montant']) ? htmlspecialchars(strtolower(trim($_POST['montant']))) : '';
+              $types = isset($_POST['types']) ? htmlspecialchars(strtolower(trim($_POST['types']))) : '';
+              $compt_id = isset($_POST['compt_id']) ? htmlspecialchars(strtolower(trim($_POST['compt_id']))) : '';
+            
+          
+              if ($montant && $types && $compt_id) {
+                  $insertsql = "INSERT INTO transactions(montant,types,compt_id)
+                                VALUES('$montant','$types','$compt_id')";
+                  mysqli_query($cnx, $insertsql);
+                  echo "Valid";
+              } else {
+                  // echo "Veuillez saisir tous les champs";
+              }
+          }
+
+
+                
+
+
+                ?>
 
 
 
@@ -54,7 +104,7 @@ include("navbar.php");
             <label for="montant" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">MONTANT</label>
           </div>
           <div class="relative z-0 w-full mb-5 group">
-            <input type="text" name="type" id="type" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+            <input type="text" name="types" id="type" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
             <label for="type" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">TYPE</label>
           </div>
           <div class="grid md:grid-cols-2 md:gap-6">
@@ -64,7 +114,7 @@ include("navbar.php");
             </div>
             
           <div class="flex justify-center">
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-14 py-1 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            <button type="submit" name="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-14 py-1 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
           </div>
         </form>
       </div>
