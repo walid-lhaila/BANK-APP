7e14dc0d9ee748d19e5abbd09b41076938b591cd
walid-lhaila <walidlhaila00@gmail.com>
2023-11-26 18:06:32 +0100

    modi6

--- bank.php
+++ bank.php
@@ -19,10 +19,10 @@
           
 
             if($nom && $logo){
-              $insertsql "INSERT INTO bank(nom,logo) 
+              $insertsql = "INSERT INTO bank(nom,logo) 
               VALUES ('$nom', '$logo')";
               mysqli_query($cnx, $insertsql);
-              echo "insert valid";
+              // echo "insert valid";
             }else{
               echo "invalid insert";
             }
@@ -58,7 +58,7 @@ include("navbar.php");
     <button id="cardbank" class="font-bold mt-10 ml-10 px-5 py-1 border-3 shadow-md transition ease-in duration-500 border-blue-300 dark:bg-gray-700 text-gray-200  font-serif ">+ Add Bank</button>
 
 <div class="relative ml-[40px] top-10">
-    <table class="w-[900px] text-sm text-left rtl:text-right text-gray-200 dark:text-gray-200">
+    <table class="w-[900px] text-center text-sm text-left rtl:text-right text-gray-200 dark:text-gray-200">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-200">
             <tr>
                 <th scope="col" class="px-6 py-3">ID</th>
@@ -84,7 +84,7 @@ include("navbar.php");
                   <td> {$row['logo']}</td>
                   <td>
                             <a href='{$row["id"]}' class='font-bold text-white h-8 rounded cursor-pointer px-3 bg-gray-700 shadow-md transition ease-out duration-500 border-gray-700 '>EDIT</a>
-                            <a href='delet_distributeur.php?id={$row["id"]}' class='font-bold text-white h-8 rounded  cursor-pointer px-2 bg-red-700 shadow-md transition ease-out duration-500 border-red-700 '>DELET</a>
+                            <a href='delet_bank.php?id={$row["id"]}' class='font-bold text-white h-8 rounded  cursor-pointer px-2 bg-red-700 shadow-md transition ease-out duration-500 border-red-700 '>DELET</a>
                   </td>
                   </tr>";
                 }
@@ -106,10 +106,7 @@ include("navbar.php");
           <div class="flex justify-end">
           <a href="bank.php"><img class="h-[15px]" src="icons8-close-50.png" alt="" ></a>
           </div>
-          <div class="relative z-0 w-full mb-5 group">
-            <input type="text" name="id" id="id" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
-            <label for="id" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">ID</label>
-          </div>
+          
           <div class="relative z-0 w-full mb-5 group">
             <input type="text" name="nom" id="nom" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
             <label for="nom" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">NOM</label>
---
+++ delet_bank.php
@@ -0,0 +1,20 @@
+<?php
+    include("datacnx.php");
+
+        if(isset($_GET["id"])){
+            $id = $_GET["id"];
+
+        $deletsql = "DELETE FROM bank WHERE id ='$id'" ;
+            if(mysqli_query($cnx, $deletsql)){
+                echo "bank deleted successfully";
+            }else{
+                echo "bank does not deleted successfully";
+            }
+        
+        }
+        header("location: bank.php");
+        exit();
+
+
+
+?>
