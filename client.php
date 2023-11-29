7c468388b9ccb221a654f095ae6a95a7a2eb456d
walid-lhaila <walidlhaila00@gmail.com>
2023-11-29 10:20:44 +0100

    role

--- client.php
+++ client.php
@@ -15,6 +15,7 @@
             username VARCHAR(30) NOT NULL,
             pass VARCHAR(30) NULL,
             agency_id INT(30) UNSIGNED NOT NULL,
+
             FOREIGN KEY (agency_id) REFERENCES agency(id)
             ON DELETE CASCADE 
             ON UPDATE CASCADE
@@ -178,6 +179,11 @@
             <div class="relative z-0 w-full mb-5 group">
               <input type="text" name="agency_id" id="agency_id" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
               <label for="agency_id" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">AGENCY ID</label>
+            </div>
+            <div class="relative z-0 w-full mb-5 group">
+              <select name="" id="">
+                <option value=""></option>
+              </select>
             </div>
           </div>
           <div class="flex justify-center">
